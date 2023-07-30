21078b75852bd771579da813e61a92c070c7cabf
Do U Min <aishi1026@gmail.com>
2023-07-31 01:55:49 +0900

    기본구조

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    implementation ("com.google.android.material:material:1.4.0.")
+    implementation ("com.android.support:design:28.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
